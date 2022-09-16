f47d64d791150a4b017f4eaff4d08b49bb44d3eb
ip07 <78528552+its-pablo@users.noreply.github.com>
2022-09-16 15:36:08 -0700

    rewrite

--- src/src.js
+++ src/src.js
@@ -1,38 +1,5 @@
-class litb {
-    constructor({ container: container = document.body, commands: { ...commandList } } = {}) {
-        container.style.backgroundColor = 'black'
-        let input = document.createElement('input')
-        container.appendChild(input)
-        this.inputEl = input
-        this.containerEl = container
-        input.addEventListener('keypress', e => {
-            if (event.key === "Enter") {
-                let command;
-                commandList.forEach(command => {
-                    if (e.target.value.startsWith(command.name)) {
-                        command = command.name
-                    }
-                })
-                if (command) {
-                    this.print('command found; awesome!') //TODO
-                } else {
-                    this.print(`${e.target.value}: command not found`)
-                }
-            }
-        })
-    }
-    print(arg) {
-        let div = document.createElement('div')
-        div.innerHTML = arg
-        div.insertBefore(this.inputEl)
+class parser {
+    constructor(){
+        
     }
 }
-
-// application of litb
-
-let Instance = new litb({
-    commands: [{
-        name: "git",
-        //the rest is tbd (: have fun
-    }]
-})
